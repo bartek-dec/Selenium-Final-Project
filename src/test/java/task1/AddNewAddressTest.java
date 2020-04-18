5419145b3fa2f754e89d39ca22b9df21474f4b9a
Bartlomiej Dec <decbartek@gmail.com>
2020-04-18 21:52:12 +0200

    added missing slash

--- src/test/java/task1/AddNewAddressTest.java
+++ src/test/java/task1/AddNewAddressTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/cucumber/task1",
+@CucumberOptions(features = "src/cucumber/task1/",
         plugin = {"pretty", "html:out"})
 public class AddNewAddressTest {
 }
