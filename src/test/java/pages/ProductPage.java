eec92b09001f8bd2342c8e0e1e5a9f88aa48e2e4
Bartlomiej Dec <decbartek@gmail.com>
2020-04-22 21:25:13 +0200

    added WebDriverWait to ProductPage class

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductPage {
 
@@ -48,6 +50,13 @@ public class ProductPage {
     }
 
     public void proceedToCheckout() {
-        proceedButton.click();
+        getProceedButton().click();
+    }
+
+    private WebElement getProceedButton() {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(proceedButton));
+
+        return button;
     }
 }
