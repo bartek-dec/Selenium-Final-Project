960cf64f72d343dd6e0fe4ead1e6427b5f44b4c8
Bartlomiej Dec <decbartek@gmail.com>
2020-04-22 21:24:16 +0200

    added properties file and ConfigFileReader class

---
+++ src/test/java/dataProvider/ConfigFileReader.java
@@ -0,0 +1,72 @@
+package dataProvider;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigFileReader {
+
+    private final String propertyFilePath = "configs/configuration.properties";
+
+    private Properties properties;
+
+    public ConfigFileReader() {
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+
+            try (reader) {
+                properties.load(reader);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("configuration.properties not found at " + propertyFilePath);
+        }
+    }
+
+    public String getSysPropertyName() {
+        String sysPropertyName = properties.getProperty("sysPropertyName");
+
+        if (sysPropertyName != null) {
+            return sysPropertyName;
+        } else {
+            throw new RuntimeException("sysPropertyName not specified in the configuration.properties file");
+        }
+    }
+
+    public String getSysPropertyValue() {
+        String sysPropertyValue = properties.getProperty("sysPropertyValue");
+
+        if (sysPropertyValue != null) {
+            return sysPropertyValue;
+        } else {
+            throw new RuntimeException("sysPropertyValue not specified in the configuration.properties file");
+        }
+    }
+
+    public long getImplicitlyWait() {
+        String implicitlyWait = properties.getProperty("implicitlyWait");
+
+        if (implicitlyWait != null) {
+            return Long.parseLong(implicitlyWait);
+        } else {
+            throw new RuntimeException("implicitlyWait not specified in the configuration.properties file");
+        }
+    }
+
+    public String getUrl() {
+        String url = properties.getProperty("url");
+
+        if (url != null) {
+            return url;
+        } else {
+            throw new RuntimeException("url not specified in the configuration.properties file");
+        }
+    }
+}
